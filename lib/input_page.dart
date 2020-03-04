d40d7d0c784e09cd4456390a7e4077026a48a447
MtDeity <o8olo853q43@gmail.com>
2020-03-04 18:23:45 +0900

    add a bottom container

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -50,6 +52,14 @@ class _InputPageState extends State<InputPage> {
                 ),
               ],
             ),
+          ),
+          Container(
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(
+              top: 10.0,
+            ),
+            height: bottomContainerHeight,
+            width: double.infinity,
           ),
         ],
       ),
