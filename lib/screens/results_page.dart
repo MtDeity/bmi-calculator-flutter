af0ebcf3dff9fa4e494a769866773e3015105dc9
MtDeity <o8olo853q43@gmail.com>
2020-03-05 20:17:09 +0900

    create directories

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -1,6 +1,5 @@
+import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:bmi_calculator/constants.dart';
-import 'package:bmi_calculator/input_page.dart';
-import 'package:bmi_calculator/reusable_card.dart';
 import 'package:flutter/material.dart';
 
 class ResultsPage extends StatelessWidget {
